7b0ae6f5fab5ddb2bfa3407451ea80ad33d6b19a
MrRiegel <ichhabads@gmail.com>
2017-01-15 19:29:11 +0100

    first build

--- src/main/java/mrriegel/sharedpotions/SharedPotions.java
+++ src/main/java/mrriegel/sharedpotions/SharedPotions.java
@@ -1,15 +1,17 @@
-package mrriegel.limelib;
+package mrriegel.sharedpotions;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
@@ -17,16 +19,14 @@ import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.commons.lang3.ArrayUtils;
 
 import com.google.common.collect.Sets;
 
-@Mod(modid = SharedPotions.MODID, name = SharedPotions.NAME, version = SharedPotions.VERSION)
+@Mod(modid = SharedPotions.MODID, name = SharedPotions.NAME, version = SharedPotions.VERSION, acceptableRemoteVersions = "*")
 @EventBusSubscriber
 public class SharedPotions {
 
@@ -36,8 +36,6 @@ public class SharedPotions {
 	public static final String VERSION = "1.0.0";
 	public static final String NAME = "Shared Potions";
 	public static final String MODID = "sharedpotions";
-
-	public static final Logger log = LogManager.getLogger(NAME);
 
 	//config
 	public static Configuration config;
@@ -50,15 +48,12 @@ public class SharedPotions {
 		config = new Configuration(event.getSuggestedConfigurationFile());
 		onlyPlayer = config.getBoolean("onlyPlayer", Configuration.CATEGORY_GENERAL, true, "Potions will be shared only with players.");
 		onlyPositive = config.getBoolean("onlyPositive", Configuration.CATEGORY_GENERAL, true, "Only positive potions will be shared.");
-		global = config.getBoolean("global", Configuration.CATEGORY_GENERAL, false, "Share potions with the whole dimension (only with players).");
-		range = config.getInt("range", Configuration.CATEGORY_GENERAL, 5, 1, 100, "The range potions will be shared within");
-		type = config.getString("type", Configuration.CATEGORY_GENERAL, "auto", "Type of sharing."+Configuration.NEW_LINE+"\"auto\": automatically for nearby entities,"+Configuration.NEW_LINE+"\"hit\": you need to hit an entity with any potion", new String[] { "auto", "hit" });
+		global = config.getBoolean("global", Configuration.CATEGORY_GENERAL, false, "Share potions with the whole dimension (only with players, for type auto).");
+		range = config.getInt("range", Configuration.CATEGORY_GENERAL, 8, 1, 100, "The range potions will be shared within");
+		type = config.getString("type", Configuration.CATEGORY_GENERAL, "auto", "Type of sharing." + Configuration.NEW_LINE + "\"auto\": automatically for nearby entities," + Configuration.NEW_LINE + "\"hit\": you need to hit an entity with any potion (or glass bottle) in your hand", new String[] { "auto", "hit" });
+		//		hitItems=config.getStringList("hitItems", Configuration.CATEGORY_GENERAL, new String[]{"potion"}, "Items you need in your hand to share potions (for type \"hit\").");
 		if (config.hasChanged())
 			config.save();
-	}
-
-	@Mod.EventHandler
-	public void init(FMLInitializationEvent event) {
 	}
 
 	private static void sharePotion(EntityPlayer player, EntityLivingBase entity) {
@@ -66,8 +61,10 @@ public class SharedPotions {
 		//		if (SharedPotions.onlyPositive)
 		//			potions = Iterables.filter(potions, (PotionEffect pe) -> !pe.getPotion().isBadEffect());
 		for (PotionEffect pe : potions)
-			if (!SharedPotions.onlyPositive || !pe.getPotion().isBadEffect())
-				entity.addPotionEffect(PotionEffect.readCustomPotionEffectFromNBT(pe.writeCustomPotionEffectToNBT(new NBTTagCompound())));
+			if (!SharedPotions.onlyPositive || !pe.getPotion().isBadEffect()) {
+				PotionEffect p = PotionEffect.readCustomPotionEffectFromNBT(pe.writeCustomPotionEffectToNBT(new NBTTagCompound()));
+				entity.addPotionEffect(p);
+			}
 	}
 
 	@SubscribeEvent
@@ -77,8 +74,7 @@ public class SharedPotions {
 		if (!event.getEntityLiving().worldObj.isRemote && event.getEntityLiving() instanceof EntityPlayer) {
 			EntityPlayerMP player = (EntityPlayerMP) event.getEntityLiving();
 			if (player.ticksExisted % 35 == 0) {
-				Vec3d p = new Vec3d(player.posX, player.posY, player.posZ);
-				Set<EntityLivingBase> ents = Sets.newHashSet(player.worldObj.getEntitiesWithinAABB(SharedPotions.onlyPlayer ? EntityPlayer.class : EntityLivingBase.class, new AxisAlignedBB(p.addVector(SharedPotions.range, SharedPotions.range, SharedPotions.range), p.addVector(-SharedPotions.range, -SharedPotions.range, -SharedPotions.range))));
+				Set<EntityLivingBase> ents = Sets.newHashSet(player.worldObj.getEntitiesWithinAABB(SharedPotions.onlyPlayer ? EntityPlayer.class : EntityLivingBase.class, new AxisAlignedBB(player.posX + SharedPotions.range, player.posY + SharedPotions.range, player.posZ + SharedPotions.range, player.posX - SharedPotions.range, player.posY - SharedPotions.range, player.posZ - SharedPotions.range)));
 				if (SharedPotions.global)
 					ents.addAll(player.worldObj.playerEntities);
 				ents.remove(player);
@@ -94,11 +90,14 @@ public class SharedPotions {
 	public static void attack(AttackEntityEvent event) {
 		if (!SharedPotions.type.equalsIgnoreCase("hit"))
 			return;
-		if (!event.getEntityPlayer().worldObj.isRemote && event.getEntityPlayer().getHeldItemMainhand() != null && (event.getEntityPlayer().getHeldItemMainhand().getItem() == Items.POTIONITEM || event.getEntityPlayer().getHeldItemMainhand().getItem() == Items.SPLASH_POTION || event.getEntityPlayer().getHeldItemMainhand().getItem() == Items.GLASS_BOTTLE) && event.getTarget() instanceof EntityLivingBase) {
+		Item[] items = new Item[] { Items.POTIONITEM, Items.SPLASH_POTION, Items.GLASS_BOTTLE, Items.LINGERING_POTION };
+		if (!event.getEntityPlayer().worldObj.isRemote && event.getEntityPlayer().getHeldItemMainhand() != null && ArrayUtils.contains(items, event.getEntityPlayer().getHeldItemMainhand().getItem()) && event.getTarget() instanceof EntityLivingBase) {
 			EntityLivingBase entity = (EntityLivingBase) event.getTarget();
 			if (!SharedPotions.onlyPlayer || entity instanceof EntityPlayer) {
 				sharePotion(event.getEntityPlayer(), entity);
-				event.getEntityPlayer().addChatComponentMessage(new TextComponentString("Shared potions with " + entity.getDisplayName().getFormattedText() + "."));
+				event.getEntityPlayer().addChatComponentMessage(new TextComponentString("Shared potions {" + event.getEntityPlayer().getActivePotionEffects().stream().//
+						map(p -> I18n.format(p.getEffectName())).//
+						collect(Collectors.joining(", ")) + "} with " + entity.getDisplayName().getFormattedText() + "."));
 			}
 		}
 	}
